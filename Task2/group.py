e76e8d020d16800806939805a89d689ea0f01a13
DESKTOP-TCH1DCU\user <varivoda123@gmail.com>
2020-02-18 19:02:10 +0300

    Удаление ненужной папки

--- Task2/group.py
+++
@@ -1,5 +0,0 @@
-class Group:
-    def __init__(self, GroupName, GroupHeader, GroupFooter):
-        self.GroupName = GroupName
-        self.GroupHeader = GroupHeader
-        self.GroupFooter = GroupFooter

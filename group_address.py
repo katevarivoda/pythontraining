3e66981cf843246d77f2b1d5df390dc79e4bcb81
DESKTOP-TCH1DCU\user <varivoda123@gmail.com>
2020-02-18 21:08:05 +0300

    Задание №3

---
+++ group_address.py
@@ -0,0 +1,8 @@
+class Group:
+    def __init__(self, last_name, first_name, address, phone, address2, note):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.address = address
+        self.phone = phone
+        self.address2 = address2
+        self.note = note

206fccc32939143b0727cbacfe33e95cb2fa74dc
DESKTOP-TCH1DCU\user <varivoda123@gmail.com>
2020-02-18 19:08:26 +0300

    Задание 2

---
+++ group.py
@@ -0,0 +1,5 @@
+class Group:
+    def __init__(self, GroupName, GroupHeader, GroupFooter):
+        self.GroupName = GroupName
+        self.GroupHeader = GroupHeader
+        self.GroupFooter = GroupFooter
--- test_add_group.py
+++ test_add_group.py
@@ -6,66 +6,63 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
-        wd = self.wd
-        #open home page
-        wd.get("http://localhost/addressbook/group.php")
-        #login
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys("%s" % username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys("%s" % password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        #redirect to groups tab
+
+    def redirect_to_groups_tab(self, wd):
         wd.find_element_by_link_text("groups").click()
-        #creating groups
+
+    def create_groups(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Test1")
+        wd.find_element_by_name("group_name").send_keys(group.GroupName)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Test")
+        wd.find_element_by_name("group_header").send_keys(group.GroupHeader)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Footer")
+        wd.find_element_by_name("group_footer").send_keys(group.GroupFooter)
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Test2")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Test")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Footer")
-        wd.find_element_by_name("submit").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.redirect_to_groups_tab(wd)
+        self.create_groups(wd, Group(GroupName="Test1", GroupHeader="Test", GroupFooter="Footer"))
+        self.logout(wd)
+
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.redirect_to_groups_tab(wd)
+        self.create_groups(wd, Group(GroupName="", GroupHeader="", GroupFooter=""))
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()

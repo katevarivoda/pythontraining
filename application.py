5dec37086dca1bcdb8cd602ba4cd233d0eb0f1bc
DESKTOP-TCH1DCU\user <varivoda123@gmail.com>
2020-02-21 12:28:51 +0300

    Задание №4

---
+++ application.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+
+
+class Application:
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("%s" % username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("%s" % password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def redirect_to_groups_tab(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def create_groups(self, group):
+        wd = self.wd
+        self.redirect_to_groups_tab()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.GroupName)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.GroupHeader)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.GroupFooter)
+        wd.find_element_by_name("submit").click()
+        self.redirect_to_groups_tab()
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def destroy(self):
+        self.wd.quit()

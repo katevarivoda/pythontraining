d0dfda8dbe2979fb0f604675eb51c84781a23670
DESKTOP-TCH1DCU\user <varivoda123@gmail.com>
2020-03-06 14:31:01 +0300

    Оптимизирован переход между страницами

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,7 +7,8 @@ class ContactHelper:
 
     def redirect_to_contacts_tab(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("addressbook.php") and len(wd.find_elements_by_name("selected[]")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def redirect_to_home_page(self):
         wd = self.app.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,4 +8,3 @@ def test_add_group(app):
 
 def test_add_empty_group(app):
     app.group.create(Group(GroupName="", GroupHeader="", GroupFooter=""))
-

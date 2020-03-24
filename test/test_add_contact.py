93990e4d9925923141ad70395df6ac0e9cddda0e
DESKTOP-TCH1DCU\user <varivoda123@gmail.com>
2020-03-24 13:28:33 +0300

    Реализована загрузка данных о контактах из json

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,26 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(last_name="", first_name="", homephone="", work="", mobile="", email="", email2="")] + [
-    Contact(last_name=random_string("last_name", 15), first_name=random_string("first_name", 15),
-            homephone=random_string("homephone", 10), work=random_string("work", 10),
-            mobile=random_string("mobile", 15),
-            email=random_string("email", 15), email3=random_string("email2",15))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.add_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()

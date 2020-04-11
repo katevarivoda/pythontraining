0367a24366a14201ea2d9a345020cff8f7b4d689
DESKTOP-TCH1DCU\user <varivoda123@gmail.com>
2020-04-11 19:36:13 +0300

    Доработана ORM фикстура для выявления контакта в группе

--- check_db_connection.py
+++ check_db_connection.py
@@ -2,11 +2,12 @@ __author__ = 'Ekaterina'
 
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_group_list()
+    l = db.get_contacts_in_group(Group(id="159"))
     for item in l:
         print(item)
     print(len(l))
--- fixture/orm.py
+++ fixture/orm.py
@@ -5,6 +5,7 @@ from _datetime import datetime
 from pony.orm import *
 import pymysql
 
+from model.contact import Contact
 from model.group import Group
 
 
@@ -17,6 +18,8 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups",
+                       lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -24,17 +27,35 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts",
+                     lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password)
         self.db.generate_mapping()
+        sql_debug(True)
 
     def convert_groups_to_model(self, groups):
         def convert(group):
-            return Group(id=str(group.id), GroupName=group.name,GroupHeader=group.header, GroupFooter=group.footer)
+            return Group(id=str(group.id), GroupName=group.name, GroupHeader=group.header, GroupFooter=group.footer)
+
         return list(map(convert, groups))
 
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), first_name=contact.firstname, last_name=contact.lastname)
+
+        return list(map(convert, contacts))
 
     @db_session
     def get_group_list(self):
         return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
